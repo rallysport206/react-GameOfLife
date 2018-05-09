7efcbd0ad98401b2855aeb8518209a68376bb799
rallysport206 <phan.tony125@gmail.com>
2018-05-09 15:16:11 -0700

    first commit with Game.js added

---
+++ src/Game.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '.Game.css';
+
+const CELL_SIZE = 20;
+const WIDTH = 800;
+const HEIGHT = 600;
+
+class Game extends React.Componet{
+    render() {
+        return (
+            <div>
+                <div className="Board"
+                    style={{width: WIDTH, height: HEIGHT}}>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Game;
